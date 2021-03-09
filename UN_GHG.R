3c237d4225094506509e5ec6393d502af70d8189
Y-Tamba <tambayvonne@gmail.com>
2021-03-09 23:58:30 +0300

    data cleanup

--- UN_GHG.R
+++ UN_GHG.R
@@ -1,7 +1,5 @@
 library(readr)
 GHG <- read_csv("greenhouse_gas_inventory_data_data.csv")
-View(GHG)
-class(GHG)
 
 install.packages(c("knitr", "rmarkdown"))
 install.packages("ymlthis")
@@ -9,17 +7,15 @@ install.packages("ymlthis")
 rmarkdown::render(
   input  = 'UN_GHG.Rmd'
   , params = list(
-    name = 'EDA Report',
-    data = 'green_house_gas_inventory_data_data.csv'
+    data = "green_house_gas_inventory_data_data.csv"
     ,  date     = "r format(Sys.time(), '%d %B, %Y')"
   )
 )
 
-GHG <- readr::read_csv("greenhouse_gas_inventory_data_data.csv")
 
-yml()
-yml() %>% 
-  yml_output(pdf_document())
 
-  yml_output(pdf_document())
-  
+##using ymlthis
+library(ymlthis) 
+yml_output(pdf_document('UN_GHG.Rmd'))
+yml(.yml, 'UN_GHG.Rmd') 
+ 
